12b68289b430a88db074cb2f48c7a1b36dfd8065
RosieCarvalho <rosie.baltazar.carvalho@gmail.com>
2022-08-08 15:58:19 -0300

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import useWebSocket from 'react-use-websocket';
+
+function App() {
+  const { lastJsonMessage, sendMessage } = useWebSocket('ws://localhost:3000', {
+  onOpen: () => console.log(`Connected to App WS`),
+  onMessage: () => {
+    if (lastJsonMessage) {
+      console.log(lastJsonMessage);
+    }
+  },
+  queryParams: { 'token': '123456' },
+  onError: (event) => { console.error(event); },
+  shouldReconnect: (closeEvent) => true,
+  reconnectInterval: 3000,
+
+});
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
